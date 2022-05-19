4be363bb3a9443ab9e78a0a383fcddefb1fce966
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:05:05 +0700

    Add files via upload

---
+++ tugas3_2.php
@@ -0,0 +1,12 @@
+<?php 
+	$x = 15000;
+	$y = 12.5;
+	$z = $x * 12;
+	$a = $z * $y;
+
+	echo "<h5>Tabungan Awal = Rp.$x </h4>";
+	echo "<h5>Bunga Setahun $y% </h5>";
+	echo "<h5>Total Tabungan Setahun = $a </h5>";
+	echo "<h5>Jawaban = Rp.$a </h5>";
+
+?>
